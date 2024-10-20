6f6fddfde82aee0a075f840475d943d996288811
mariazakria <marizakria130@gmail.com>
2024-10-20 23:42:24 +0300

    jquery

---
+++ assets/js/main.js
@@ -0,0 +1,76 @@
+"use strict"
+$(".sideNav").on("click",function(){
+        $(".sidebar").animate({width: "250px"},1000),
+        $(".sideNav").animate({ left: "262px" }, 1000); 
+
+})
+
+
+$(".toggle").on("click",function(){
+    $('.inner').not($(this).next()).slideUp(500);
+        $(this).next().slideToggle(500);
+})
+
+$(".closebtn").on("click",function(){
+    $(".sidebar").animate({width: "0px"},1000),
+    $(".sideNav").animate({ left: "25px" }, 1000); 
+
+})
+const countDownDate = new Date("dec 31, 2024 00:00:00").getTime();
+
+var downCounter = setInterval(function () {
+
+  var now = new Date().getTime();
+  var timeleft = countDownDate - now;
+
+  var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+  if (days < 9)
+    $('.days').text('0' + days + ' D')
+  else
+    $('.days').text(days + ' D')
+
+  if (hours < 9)
+    $('.hours').text('0' + hours + ' H')
+  else
+    $('.hours').text(hours + ' H')
+
+  if (minutes < 9)
+    $('.minutes').text('0' + minutes + ' M')
+  else
+    $('.minutes').text(minutes + ' M')
+
+  if (seconds < 9)
+    $('#seconds').text('0' + seconds + ' S')
+  else
+    $('#seconds').text(seconds + 'S')
+
+  if (timeleft < 0) {
+    clearInterval(myfunc);
+    document.querySelector(".days").innerHTML = ""
+    document.querySelector(".hours").innerHTML = ""
+    document.querySelector(".mins").innerHTML = ""
+    document.querySelector(".secs").innerHTML = ""
+    document.querySelector(".end").innerHTML = "PARTY TIME!!";
+  }
+
+}, 1000);
+
+
+
+$("#textarea").keyup(function () {
+    let val = $("#textarea").val().length;
+    let remaining = 100 - val;
+
+    if (val >= 100) {
+        $("#textarea").val($("#textarea").val().substring(0, 100));  
+        $("#btnForm").addClass("disabled").css("background-color", "#E68285");
+    } else {
+        $("#btnForm").removeClass("disabled").css("background-color", "#D62E33");
+    }
+
+    $(".counter").text(remaining);
+});
